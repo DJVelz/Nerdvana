9fb65046b5cda620b1cb128d51a7269a3d111b15
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-02 18:53:47 -0400

    Changed nav bar color

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const { isSeller, router } = useAppContext();
 
   return (
-    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
+    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700 navbar-color-purple">
       <Link href="/" className="hover:text-gray-900 transition">
           Nerdvana
         </Link>
