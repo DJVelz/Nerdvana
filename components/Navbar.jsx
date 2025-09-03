7d56a7833c304771dc01ed27b6ed7d2d8101fa0c
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-02 21:24:29 -0400

    Removed traces of seller

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,8 +7,6 @@ import Image from "next/image";
 
 const Navbar = () => {
 
-  const { isSeller, router } = useAppContext();
-
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700 navbar-color-purple">
       <Link href="/" className="hover:text-gray-900 transition">
@@ -39,7 +37,6 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center md:hidden gap-3">
-        {isSeller && <button onClick={() => router.push('/seller')} className="text-xs border px-4 py-1.5 rounded-full">Seller Dashboard</button>}
         <button className="flex items-center gap-2 hover:text-gray-900 transition">
           <Image src={assets.user_icon} alt="user icon" />
           Account
