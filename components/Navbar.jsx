cbb3142c3f47d85773cfaa7c0e1ab68938f7aeb2
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-08 14:26:49 -0400

    Navbar text white

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,21 +8,21 @@ import Image from "next/image";
 const Navbar = () => {
 
   return (
-    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-white-700 bg-[#471396]">
-      <Link href="/" className="hover:text-white-900 transition">
+    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-white bg-[#471396]">
+      <Link href="/" className="hover:text-white transition">
           Nerdvana
         </Link>
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
-        <Link href="/" className="hover:text-white-900 transition">
+        <Link href="/" className="hover:text-white transition">
           Home
         </Link>
-        <Link href="/all-products" className="hover:text-white-900 transition">
+        <Link href="/all-products" className="hover:text-white transition">
           Shop
         </Link>
-        <Link href="/" className="hover:text-white-900 transition">
+        <Link href="/" className="hover:text-white transition">
           About Us
         </Link>
-        <Link href="/" className="hover:text-white-900 transition">
+        <Link href="/" className="hover:text-white transition">
           Contact
         </Link>
 
@@ -30,14 +30,14 @@ const Navbar = () => {
 
       <ul className="hidden md:flex items-center gap-4 ">
         <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
-        <button className="flex items-center gap-2 hover:text-gray-900 transition">
+        <button className="flex items-center gap-2 hover:text-white transition">
           <Image src={assets.user_icon} alt="user icon" />
           Account
         </button>
       </ul>
 
       <div className="flex items-center md:hidden gap-3">
-        <button className="flex items-center gap-2 hover:text-gray-900 transition">
+        <button className="flex items-center gap-2 hover:text-white transition">
           <Image src={assets.user_icon} alt="user icon" />
           Account
         </button>
