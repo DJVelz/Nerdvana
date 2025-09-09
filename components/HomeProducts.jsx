877209313bb2465c1f485efa0825bba9dfc8856a
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-09 16:49:00 -0400

    unhid scrollbar

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -10,7 +10,7 @@ const HomeProducts = () => {
     <div className="flex flex-col items-center pt-14">
       <p className="text-2xl font-medium text-left w-full">Products under $30</p>
       <div className="w-16 h-0.5 bg-purple rounded-full"></div>
-      <div className="flex gap-6 overflow-x-auto mt-6 pb-14 w-full scrollbar-hide">
+      <div className="flex gap-6 overflow-x-auto mt-6 pb-14 w-full">
         {products
           .filter(product => product.price < 30.0)
           .map((product, index) => (
@@ -22,7 +22,7 @@ const HomeProducts = () => {
 
       <p className="text-2xl font-medium text-left w-full">Prep for the renfair</p>
       <div className="w-16 h-0.5 bg-purple rounded-full"></div>
-      <div className="flex gap-6 overflow-x-auto mt-6 pb-14 w-full scrollbar-hide">
+      <div className="flex gap-6 overflow-x-auto mt-6 pb-14 w-full">
         {products
           .filter(product => product.category.includes("Medieval"))
           .map((product, index) => (
@@ -31,7 +31,7 @@ const HomeProducts = () => {
             </div>
           ))}
       </div>
-      
+
       <button onClick={() => { router.push('/all-products') }} className="px-12 py-2.5 border rounded text-gray-500/70 hover:bg-slate-50/90 transition">
         Shop all products
       </button>
