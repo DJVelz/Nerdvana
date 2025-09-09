b22c7da08d4223d87a17243091e8e7542cc932ca
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-09 16:34:49 -0400

    Added underlines to home page

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -9,12 +9,15 @@ const HomeProducts = () => {
   return (
     <div className="flex flex-col items-center pt-14">
       <p className="text-2xl font-medium text-left w-full">Products under $30</p>
+      <div className="w-16 h-0.5 bg-gold rounded-full"></div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-6 pb-14 w-full">
         {products
         .filter(product => product.price < 30.00)
         .map((product, index) => <ProductCard key={index} product={product} />)}
       </div>
+
       <p className="text-2xl font-medium text-left w-full">Prepare for the renaissance faire</p>
+      <div className="w-16 h-0.5 bg-gold rounded-full"></div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-6 pb-14 w-full">
         {products
         .filter(product => product.category.includes("Medieval"))
