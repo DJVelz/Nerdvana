59c38430a3f76269e9e5ee77960c6b4b730cf774
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-09 12:23:17 -0400

    Made footer white again

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer>
-      <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500 bg-purple">
+      <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500 bg-white">
         <div className="w-4/5">
           <p className="mt-6 text-sm">
             Lorem Ipsum is simply dummy text of the printing and typesetting
