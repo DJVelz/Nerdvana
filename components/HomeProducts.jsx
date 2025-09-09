c42f145ccf0c304cf891ac6a1635cbab1cd733ec
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-09 16:54:55 -0400

    moved underlines to bottom of sections

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -9,7 +9,6 @@ const HomeProducts = () => {
   return (
     <div className="flex flex-col items-center pt-14 space-y-16 w-full">
       <p className="text-2xl font-medium text-left w-full">Products under $30</p>
-      <div className="w-16 h-0.5 bg-purple rounded-full"></div>
       <div className="flex gap-6 overflow-x-auto mt-6 pb-14 w-full">
         {products
           .filter(product => product.price < 30.0)
@@ -19,9 +18,9 @@ const HomeProducts = () => {
             </div>
           ))}
       </div>
+      <div className="w-16 h-0.5 bg-purple rounded-full"></div>
 
       <p className="text-2xl font-medium text-left w-full">Prep for the renfair</p>
-      <div className="w-16 h-0.5 bg-purple rounded-full"></div>
       <div className="flex gap-6 overflow-x-auto mt-6 pb-14 w-full">
         {products
           .filter(product => product.category.includes("Medieval"))
@@ -31,6 +30,7 @@ const HomeProducts = () => {
             </div>
           ))}
       </div>
+      <div className="w-16 h-0.5 bg-purple rounded-full"></div>
 
       <button onClick={() => { router.push('/all-products') }} className="px-12 py-2.5 border rounded text-gray-500/70 hover:bg-slate-50/90 transition">
         Shop all products
