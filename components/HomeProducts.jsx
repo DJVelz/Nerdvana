1fa5425be3bf4f72d1178329e211f84d84da9ffc
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-09 16:44:01 -0400

    Changed from grid to slider

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -10,19 +10,28 @@ const HomeProducts = () => {
     <div className="flex flex-col items-center pt-14">
       <p className="text-2xl font-medium text-left w-full">Products under $30</p>
       <div className="w-16 h-0.5 bg-purple rounded-full"></div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-6 pb-14 w-full">
+      <div className="flex gap-6 overflow-x-auto mt-6 pb-14 w-full scrollbar-hide">
         {products
-        .filter(product => product.price < 30.00)
-        .map((product, index) => <ProductCard key={index} product={product} />)}
+          .filter(product => product.price < 30.0)
+          .map((product, index) => (
+            <div className="flex-shrink-0 w-64" key={index}>
+              <ProductCard product={product} />
+            </div>
+          ))}
       </div>
 
       <p className="text-2xl font-medium text-left w-full">Prep for the renfair</p>
       <div className="w-16 h-0.5 bg-purple rounded-full"></div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-6 pb-14 w-full">
+      <div className="flex gap-6 overflow-x-auto mt-6 pb-14 w-full scrollbar-hide">
         {products
-        .filter(product => product.category.includes("Medieval"))
-        .map((product, index) => <ProductCard key={index} product={product} />)}
+          .filter(product => product.category.includes("Medieval"))
+          .map((product, index) => (
+            <div className="flex-shrink-0 w-64" key={index}>
+              <ProductCard product={product} />
+            </div>
+          ))}
       </div>
+      
       <button onClick={() => { router.push('/all-products') }} className="px-12 py-2.5 border rounded text-gray-500/70 hover:bg-slate-50/90 transition">
         Shop all products
       </button>
