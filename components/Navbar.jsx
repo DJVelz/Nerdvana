71f9fdfe46cb5aa76d34e68213808b3010fce4fe
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-09 16:01:36 -0400

    Test hover on shop button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
         <Link href="/" className="hover:text-white transition">
           Home
         </Link>
-        <Link href="/all-products" className="hover:text-white transition">
+        <Link href="/all-products" className="px-3 py-2 rounded-md transition-colors duration-200 hover:bg-purple-600 hover:text-white transition">
           Shop
         </Link>
         <Link href="/" className="hover:text-white transition">
@@ -35,7 +35,6 @@ const Navbar = () => {
         <Link href="/" className="hover:text-white transition">
           Guide Forums
         </Link>
-
       </div>
 
       <ul className="hidden md:flex items-center gap-4 ">
