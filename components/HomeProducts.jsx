0eee6ddcd5e1ae5f2d4c8cd2190b668163a7b68e
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-09 12:24:54 -0400

    Changed home page button to say shop all products

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -21,7 +21,7 @@ const HomeProducts = () => {
         .map((product, index) => <ProductCard key={index} product={product} />)}
       </div>
       <button onClick={() => { router.push('/all-products') }} className="px-12 py-2.5 border rounded text-gray-500/70 hover:bg-slate-50/90 transition">
-        See more
+        Shop all products
       </button>
     </div>
   );
