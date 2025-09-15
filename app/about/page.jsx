7fcb2218d2f2fa4f5d37090521929e1865ac828d
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-15 18:08:47 -0400

    changed body text format

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -10,7 +10,7 @@ export default function About() {
             <Navbar/>
                 <main className="bg-gradient-to-r from-purple-600 to-blue-600 text-white rounded-xl shadow-lg p-8 text-center max-w-xl">
                     <h1 className="text-4xl font-bold mb-4">About Nerdvana</h1>
-                    <p>
+                    <p className="max-w-xl text-center text-lg">
                         Nerdvana is an e-commerce platform built for fans of games, tech, and all things nerdy. 
                         Our mission is to create a space where enthusiasts can find unique products, 
                         connect with their passions, and support creators.
