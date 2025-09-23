9321dee2eaef4e3b2f39c59b3599c7b4a67c5016
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-23 18:38:51 -0400

    removed unused line

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import { assets} from "@/assets/assets";
 import Link from "next/link";
-import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
 
 const Navbar = () => {
