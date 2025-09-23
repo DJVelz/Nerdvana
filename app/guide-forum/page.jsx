6cbae38b66323a70355fd0be566b5014eeb7bf5f
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-23 18:52:39 -0400

    added message to forum

--- app/guide-forum/page.jsx
+++ app/guide-forum/page.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 const Forum = () => {
-    
-}
+    return (
+        <main className="px-6 md:px-16 lg:px-32 py-8">
+            <h1 className="text-3x1 font-bold mb-6">Guide Forums</h1>
+            <p className="text-lg text-gray-700">
+                Welcome to the Nerdvana Guide Forums!
+                This is where users can share tips, guides, and discussion.
+            </p>
+        </main>
+    );
+};
+
+export default Forum;
