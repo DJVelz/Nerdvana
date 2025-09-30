d7f1a02b7f06baffb13a584476520e1f66a257ed
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-30 17:32:25 -0400

    changed indigo to mediumpurple

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
     return (
-        <section className="relative bg-gradient-to-r from-light_purple via-indigo-800 to-purple text-white py-20 px-6 md:px-16 lg:px:32">
+        <section className="relative bg-gradient-to-r from-light_purple via-mediumpurple to-purple text-white py-20 px-6 md:px-16 lg:px:32">
             <div className="max-w-4x1 mx-auto text-center">
                 {/* Headline */}
                 <h1 className="text-4x1 md:text-6x1 font-extrabold mb-6">
