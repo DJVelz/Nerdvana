0cb62a98c72a28ffc9532349def7f28447190981
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-30 17:14:36 -0400

    Added Headline

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Hero = () => {
     return (
         <section className="relative bg-gradient-to-r from-purple via-indigo-800 to-black text-white py-20 px-6 md:px-16 lg:px:32">
-
+            <div className="max-w-4x1 mx-auto text-center">
+                {/* Headline */}
+                <h1 className="text-4x1 md:text-6x1 font-extrabold mb-6">
+                    Welcome to <span className="text-purple">Nerdvana</span>
+                </h1>
+            </div>
         </section>
     );
 };
