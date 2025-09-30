d958a0bd8b9c9afd81bbe45271f1b89e62aefc6a
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-30 17:51:43 -0400

    made headline smaller for smaller screens, bigger for bigger screens

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <section className="relative bg-gradient-to-r from-light_purple via-mediumpurple to-purple text-white py-20 px-6 md:px-16 lg:px:32">
             <div className="max-w-4x1 mx-auto text-center">
                 {/* Headline */}
-                <h1 className="text-6xl md:text-xl font-extrabold mb-6">
+                <h1 className="text-4xl md:text-6xl font-extrabold mb-6">
                     Welcome to <span className="text-gold">Nerdvana</span>
                 </h1>
 
