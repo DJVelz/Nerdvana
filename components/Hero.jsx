272524c2612a778ef3aadef4e78059d320f6557a
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-30 17:11:50 -0400

    set hero dimensions

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Hero = () => {
     return (
+        <section className="relative bg-gradient-to-r from-purple via-indigo-800 to-black text-white py-20 px-6 md:px-16 lg:px:32">
 
+        </section>
     );
 };
 
