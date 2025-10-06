feadb6b2897d3cc49fce8685f22a26ccef5af0e9
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-10-06 16:49:44 -0400

    removed lorem ipsum

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,15 +6,6 @@ const Footer = () => {
   return (
     <footer>
       <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500 bg-white">
-        <div className="w-4/5">
-          <p className="mt-6 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
-        </div>
-
         <div className="w-1/2 flex items-center justify-start md:justify-center">
           <div>
             <h2 className="font-medium text-gray-900 mb-5">Company</h2>
