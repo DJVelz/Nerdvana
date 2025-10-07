7ce0e4001111e60a2c9bdc6afe5e3d9706f3306e
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-10-07 14:28:49 -0400

    added getwishlist to const

--- app/wishlist/page.jsx
+++ app/wishlist/page.jsx
@@ -3,11 +3,16 @@ import React from "react";
 import { assets } from "@/assets/assets";
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import ProductCard from "@/components/ProductCard";
 import { useAppContext } from "@/context/AppContext";
 
 const Wishlist = () => {
+  const {
+    products,
+    getWishlistProducts,
+  } = useAppContext();
 
-  const { products, router, cartItems, addToCart, updateCartQuantity, getCartCount } = useAppContext();
+  const wishlistProducts = getWishlistProducts(products); 
 
   return (
     <>
@@ -16,15 +21,15 @@ const Wishlist = () => {
         <h1 className="text-3xl font-bold mb-6">Your Wishlist</h1>
 
         {wishlistProducts.length > 0 ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-                {wishlistProducts.map((product) => (
-                    <ProductCard key={product._id} product={product} />
-                ))}
-            </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {wishlistProducts.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+          </div>
         ) : (
-            <p className="text-gray-500">Your wishlist is empty.</p>
+          <p className="text-gray-500">Your wishlist is empty.</p>
         )}
-    </div>
+      </div>
     </>
   );
 };
