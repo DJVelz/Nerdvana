472281e2b4fad2a3be01189ebf5e3cff403f79dc
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-10-14 19:17:10 -0400

    import useState and useEffect

--- app/wishlist/page.jsx
+++ app/wishlist/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 import React from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
