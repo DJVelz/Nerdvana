7e1764308642f7696161975b8a16717c6b1aaf15
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-10-21 16:25:27 -0400

    changed _id to id

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -95,10 +95,10 @@ const Product = () => {
                     </div>
 
                     <div className="flex items-center mt-10 gap-4">
-                        <button onClick={() => addToCart(productData._id)} className="w-full py-3.5 bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition">
+                        <button onClick={() => addToCart(productData.id)} className="w-full py-3.5 bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition">
                             Add to Cart
                         </button>
-                        <button onClick={() => { addToCart(productData._id); router.push('/cart') }} className="w-full py-3.5 bg-light_purple text-white hover:bg-purple transition">
+                        <button onClick={() => { addToCart(productData.id); router.push('/cart') }} className="w-full py-3.5 bg-light_purple text-white hover:bg-purple transition">
                             Buy now
                         </button>
                     </div>
