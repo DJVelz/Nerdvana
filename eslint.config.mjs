be4d4c9519b8b370057c66e278ffb11082fe9a3b
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-11-04 20:01:05 -0500

    tried restoring config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,14 @@
-import path from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-/** @type {import('eslint').Linter.Config[]} */
-const configs = [
-  ...compat.extends("next/core-web-vitals"),
-  ...compat.extends("next/typescript"),
-  
-]
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
-export default configs
+export default eslintConfig;
