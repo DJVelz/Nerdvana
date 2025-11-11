94401eaa2a261a22d3aca3723594191ff3056df3
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-11-11 17:12:10 -0500

    set up initial consts

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,3 +1,10 @@
+import { useAppContext } from "@/context/AppContext"
+import { useState } from "react"
+
 function Comments({ postId }) {
+    const { user } = useAppContext();
+    const [comments, setComments] = useState([]);
+    const [text, setText] = useState("");
+
     
 }
