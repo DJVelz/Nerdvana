bf116b95264144d3b525763cbd124db540619804
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-11-11 17:14:23 -0500

    grab comments from supabase

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,4 +1,5 @@
 import { useAppContext } from "@/context/AppContext"
+import { supabase } from "@/lib/supabaseClient";
 import { useState } from "react"
 
 function Comments({ postId }) {
@@ -6,5 +7,13 @@ function Comments({ postId }) {
     const [comments, setComments] = useState([]);
     const [text, setText] = useState("");
 
-    
+    const fetchComments = async () => {
+        const { data, error } = await supabase
+            .from("comments")
+            .select("id, content, created_at, user_id")
+            .eq("post_id", postId)
+            .order("created_at", { ascending:true });
+
+        if (!error) setComments(data);
+    };
 }
