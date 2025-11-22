a208338d4615c3e638cf6965351de583bc118a81
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-11-21 21:03:05 -0500

    avoid mismatch

--- app/wishlist/page.jsx
+++ app/wishlist/page.jsx
@@ -15,6 +15,14 @@ const Wishlist = () => {
   const router = useRouter();
 
   const { user } = useAppContext();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // don't render until client
+
 
   // Redirect if no user is logged in
   useEffect(() => {
